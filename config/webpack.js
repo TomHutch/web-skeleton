f8a96e300cfeb9f7535b0565c926fa24c5f48bb9
Tom Hutchinson <tom.hutchinson@beamly.com>
2018-03-03 19:22:49 +0000

    add initial dev env components

---
+++ config/webpack.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: path.resolve('./client'),
+  entry: './index.js',
+  output: {
+    path: path.resolve('./dist/client/js'),
+    filename: 'scripts.min.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Dev App',
+      template: 'assets/template.html',
+      filename: './../index.html',
+      inject: true,
+    }),
+  ],
+};
