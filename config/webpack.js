6af2872832b67468df8909cff0a921721866a925
Tom Hutchinson <tom.hutchinson@beamly.com>
2018-03-04 13:11:11 +0000

    use webpack-dev-server for hot reloading client

--- config/webpack.js
+++ config/webpack.js
@@ -1,15 +1,28 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  context: path.resolve('./src/client'),
-  entry: './index.js',
+  entry: {
+    app: [
+      './src/client/index.js',
+    ],
+  },
   output: {
-    path: path.resolve('./dist/client/js'),
-    filename: 'scripts.min.js',
+    path: path.resolve('./dist/client'),
+    filename: 'scripts.js',
+  },
+  devServer: {
+    contentBase: path.resolve('./dist/client'),
+    compress: true,
+    host: '0.0.0.0',
+    port: 3100,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js[x]?$/,
         exclude: /node_modules/,
@@ -20,8 +33,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Dev App',
-      template: 'assets/template.html',
-      filename: './../index.html',
+      template: 'src/client/assets/template.html',
+      filename: './index.html',
       inject: true,
     }),
   ],
